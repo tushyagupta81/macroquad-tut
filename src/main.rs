7e91c67cc6953f3bdcc0a7569a2158135b4fa440
Tushya Gupta <tushyagupta81@gmail.com>
2024-11-23 17:52:29 +0530

    squares are falling

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,61 @@
 use macroquad::prelude::*;
 
+struct Shape {
+    size: f32,
+    speed: f32,
+    x: f32,
+    y: f32,
+}
+
 #[macroquad::main("My Game!")]
 async fn main() {
-    let mut x = screen_width() / 2.0;
-    let mut y = screen_height() / 2.0;
+    const MAXSPEED: f32 = 200.0;
+    let mut squares = vec![];
+    let mut circle = Shape {
+        size: 16.0,
+        speed: MAXSPEED,
+        x: screen_width() / 2.0,
+        y: screen_height() / 2.0,
+    };
     loop {
         clear_background(DARKPURPLE);
+        let delta = get_frame_time();
+
+        if rand::gen_range(0, 99) >= 95 {
+            let size = rand::gen_range(16.0, 64.0);
+            squares.push(Shape {
+                size,
+                speed: rand::gen_range(50.0, 150.0),
+                x: rand::gen_range(size / 2.0, screen_width() / 2.0 - size / 2.0),
+                y: -size,
+            })
+        }
+
+        for square in &mut squares {
+            square.y = square.speed * delta;
+        }
+        squares.retain(|square| square.y < screen_height() + square.size);
+
+        for square in &squares {
+            draw_rectangle(square.x, square.y, square.size, square.size, GREEN);
+        }
 
         if is_key_down(KeyCode::Left) {
-            x -= 0.1;
+            circle.x -= circle.speed * delta;
         }
         if is_key_down(KeyCode::Right) {
-            x += 0.1;
+            circle.x += circle.speed * delta;
         }
         if is_key_down(KeyCode::Up) {
-            y -= 0.1;
+            circle.y -= circle.speed * delta;
         }
         if is_key_down(KeyCode::Down) {
-            y += 0.1;
+            circle.y += circle.speed * delta;
         }
-        draw_circle(x, y, 16.0, YELLOW);
+        circle.x = clamp(circle.x, 0.0, screen_width());
+        circle.y = clamp(circle.y, 0.0, screen_height());
+
+        draw_circle(circle.x, circle.y, 16.0, YELLOW);
 
         next_frame().await
     }
